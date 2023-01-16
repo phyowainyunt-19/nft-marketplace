3249232f3ad027f0a2d8970c61eeea1ecf401f00
phyowainyunt-19 <phyowainyunt.26@gmail.com>
2023-01-16 21:48:14 +0630

    Project reviewed

--- components/HomeHeader.js
+++ components/HomeHeader.js
@@ -1,7 +1,9 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Image, TextInput } from 'react-native';
+import React from 'react';
+import { COLORS, FONTS, SIZES, assets } from '../constants'
 
 const HomeHeader = () => {
+  // Project reviewed
   return (
     <View>
       <Text>HomeHeader</Text>
